7e639c639d7dd95bd2cae10592bd907166236c3e
Rohanator9000 <rkhajuria99@gmail.com>
2016-04-16 19:04:59 -0400

    Removed document ready
    It's in the popup, so unneeded

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){ 
-	$("#checkPage").click(function(){
-		var text1 = $("#search Text").val();
-		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, text);
-		});
+$("#checkPage").click(function(){
+	var text1 = $("#search Text").val();
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, text1);
 	});
 });
