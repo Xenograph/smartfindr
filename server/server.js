4c9f150729904f6863c89c8b18acdc9702a1671f
Douglas Browne <douglas.browne123@gmail.com>
2016-04-16 19:25:31 -0400

    Added synonym server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+var Express = require('express');
+var WordNet = require('node-wordnet');
+
+var port = process.env.PORT || 3000;
+
+var server = Express();
+var wordnet = new WordNet();
+
+server.get('/:word', function(req, res) {
+    wordnet.lookup(req.params.word, function(results) {
+        var synonyms = new Set();
+        results.forEach(function(result) {
+            result.synonyms.forEach(function(synonym) {
+                synonyms.add(synonym.replace(/_/g, ' ')); 
+            });
+        });
+        res.send([...synonyms]);
+    });
+});
+
+server.listen(port, function() {
+    console.log('smartfindr_svr listening on port ' + port);
+})
