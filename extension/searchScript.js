3622768e98055eb1528777a4a0387a3078e200cc
Rohanator9000 <rkhajuria99@gmail.com>
2016-04-17 01:43:45 -0400

    Edited Search Code
    Edited the search code copied from online

--- extension/searchScript.js
+++ extension/searchScript.js
@@ -1,37 +1,19 @@
 function arrayOutput(element, index, array) {
-	
-	var TRange=null;
+	var TRange = null;
 	if (parseInt(navigator.appVersion)<4)
 		return;
 	var strFound;
-	if (window.find) {
+	if(window.find) {
 		// CODE FOR BROWSERS THAT SUPPORT window.find
 		strFound = self.find(element);
-		if (!strFound) {
-			strFound=self.find(element,0,1);
-			while (self.find(element,0,1))
+		if(!strFound) {
+			strFound = self.find(element,0,1);
+			while(self.find(element,0,1))
 				continue;
 		}
-	} else if (navigator.appName.indexOf("Microsoft")!=-1) {
-		// EXPLORER-SPECIFIC CODE
-		if (TRange!=null) {
-			TRange.collapse(false);
-			strFound=TRange.findText(element);
-			if (strFound)
-				TRange.select();
-		}
-		if (TRange==null || strFound==0) {
-			TRange=self.document.body.createTextRange();
-			strFound=TRange.findText(element);
-			if (strFound)
-				TRange.select();
-		}
-	} else if (navigator.appName=="Opera") {
-		alert("Opera browsers not supported, sorry...");
-		return;
 	}
-	if (!strFound)
-		alert("String '"+element+"' not found!");
+	if(!strFound)
+		alert("String '" + element + "' not found!");
 }
  
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
