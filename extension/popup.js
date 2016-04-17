3cb0c808465c8d60f77193da8dbbdc5d407e45da
Rohanator9000 <rkhajuria99@gmail.com>
2016-04-16 21:01:05 -0400

    Removed Jquery  + changed script calls
    Jquery actually doesn't work. Moved the script source calls to after
    body element initialization.

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,8 @@
-$("#checkPage").click(function(){
-	var text1 = $("#search Text").val();
+ function hello() {
+	var userText = document.getElementById('searchText').value;
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, text1);
+		chrome.tabs.sendMessage(tabs[0].id, userText);
 	});
-});
+}
+
+document.getElementById('checkPage').addEventListener('click', hello);
