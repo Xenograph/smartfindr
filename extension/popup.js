25f5f83c56ef92fa0c00c6acbe8be6c5acffb209
Rohanator9000 <rkhajuria99@gmail.com>
2016-04-16 21:55:58 -0400

    Added synonym functionality
    Accessed server to receive synonyms of word

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,15 @@
  function hello() {
 	var userText = document.getElementById('searchText').value;
+	
+	var xmlHttp = new XMLHttpRequest();
+	var url = "http://smartfindr-server.mybluemix.net/" + userText;
+	console.log(url);
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    var synonyms = xmlHttp.responseText;
+	
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, userText);
+		chrome.tabs.sendMessage(tabs[0].id, synonyms);
 	});
 }
-
 document.getElementById('checkPage').addEventListener('click', hello);
